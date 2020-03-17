9725d3551ad1118cfb6cb6707ba56c768d63d973
Alejandro Oropeza <alejandro_oropeza@gap.com>
2020-03-17 16:35:59 -0600

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['airbnb', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx']
+      }
+    ],
+    'react/prop-types': 0,
+    'no-underscore-dangle': 0,
+    'import/imports-first': ['error', 'absolute-first'],
+    'import/newline-after-import': 'error',
+    'import/prefer-default-export': 'off'
+  }
+}
