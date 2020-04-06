92579f326ccb9724a2510f88e7eba5d47037053d
ProximateApps <aoropeza@proximateapps.com>
2020-04-05 23:25:38 -0500

    Added reach/route ᕦ╏^^╏ᕤ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { SceneSearch } from './components/Scenes/Search'
+import { SceneAbout } from './components/Scenes/About/index'
 import { Logo } from './components/UI/Logo'
 
 export const App = () => (
   <>
     <GlobalStyles />
     <Logo />
-    <SceneSearch />
+    <Router>
+      <SceneSearch path="/" />
+      <SceneAbout path="/about" />
+    </Router>
   </>
 )
